c2b95bf30f23485f5016bd92b222cd079e76de01
Logan White <55511399+ThyLAW@users.noreply.github.com>
2020-09-26 21:00:59 -0400

    Final Update
    Completed difficulty, abstraction, main menu, ui elements, and added music.

---
+++ Apple Picker Prototype/Assets/Scripts/TextInactiveScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextInactiveScript : MonoBehaviour
+{ 
+    public GameObject textInstructions;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        
+        textInstructions = GameObject.Find("/Canvas/settingsMenu/infoText");
+        textInstructions.gameObject.SetActive(false);
+    }
+
+}
